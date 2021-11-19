a655eb1845fdfe4a2e4bc26c9c49ffe3dc670ad9
Humaida Ara <57634386+humaidaAra@users.noreply.github.com>
2021-11-19 03:23:00 -0800

    Updated assignment.py

--- assignment.py
+++ assignment.py
@@ -1,9 +1,16 @@
 def check_lasts(lists):             #this method does all the work
     for i in lists:
-        x = find_max(i)             #return INDEX of max element inside the sub list
-        if x == len(i) - 1:         #check if found element is the last element
-            tmp = factorial(i[x])   #if yes... find factorial value and replace it with original
-            i[x] = tmp
+
+
+        # way 1
+        i[-1] = factorial(i[-1])
+        
+        # way 2
+        
+        #x = find_max(i)             #return INDEX of max element inside the sub list
+        # if x == len(i) - 1:         #check if found element is the last element
+        #     tmp = factorial(i[x])   #if yes... find factorial value and replace it with original
+        #     i[x] = tmp
 
 
 def factorial(x):                   #finds factorial of a number where the number is NOT 0
@@ -20,6 +27,8 @@ def find_max(subl):                #finds the max element of a sublist
             sam = i
 
     return sam
+
+
 
 
 the_set = [[1, 12, 3], [14, 5, 16], [7, 8, 9], [10, 11, 12], [13, 14, 15]]
